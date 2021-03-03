d11b5f4620e633521b8c0d070786a053871fc083
Karimah Swan <karimahswan@gmail.com>
2021-03-03 10:34:03 -0500

    Completed steps 7-8

--- app/views.py
+++ app/views.py
@@ -32,36 +32,36 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST": #and form.validate_on_submit():
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data: #and form.password.data:
+        if form.username.data and form.password.data:
             # Get the username and password values from the form.
-            # username=form.username.data
-            # password=form.password.data
+            username=form.username.data
+            password=form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
-            #user=UserProfile.query.filter_by(username=username,password=password).first()
+            user=UserProfile.query.filter_by(username=username,password=password).first()
 
-            #if user is not None and check_password_hash(user.password,password):
-                #remember_me=False
+            if user is not None and check_password_hash(user.password,password):
+                remember_me=False
 
-                #if 'remember_me' in request.form:
-                    #remember_me=True
+                if 'remember_me' in request.form:
+                    remember_me=True
 
             # get user id, load into session
-            login_user(user)#, remember=remember_me)
+            login_user(user, remember=remember_me)
 
             # remember to flash a message to the user
-            #flash('Login Successfull.','success')
-            #return redirect(url_for("secure_page"))
+            flash('Login Successfull.','success')
+            return redirect(url_for("secure_page"))
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
-        #else:
-            #flash('Username or Password is incorrect.','danger')
+        else:
+            flash('Username or Password is incorrect.','danger')
 
     return render_template("login.html", form=form)
 
